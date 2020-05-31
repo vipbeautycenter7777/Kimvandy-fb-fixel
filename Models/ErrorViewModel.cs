99c3f157e9ec59142d07810332a8d132a26c408b
KimVandy <63632396+KimVandy@users.noreply.github.com>
2020-05-31 22:41:21 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kimvandy_fb_fixel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
